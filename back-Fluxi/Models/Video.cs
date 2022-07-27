282741380826c117415a72453b4120f56ac739e8
MoncheauxJosse <lordssly@gmail.com>
2022-07-27 14:15:42 +0200

    modification acteur

--- back-Fluxi/Models/Video.cs
+++ back-Fluxi/Models/Video.cs
@@ -5,17 +5,23 @@ namespace back_Fluxi.Models
     [Table("video")]
     public class Video
     {
+
         private int id;
+        private string name;
+        private int categorieId;
+        private string acteurId;
+
+        [Column("id")]
+        public int Id { get => id; set => id = value; }
+
+        [Column("name")]
+        public string Name { get => name; set => name = value; }
 
         [Column("categorie_id")]
-        public int CategorieId { get ; set; }
+        public int CategorieId { get => categorieId; set => categorieId = value; }
 
         [ForeignKey("CategorieId")]
         public Categorie Categorie { get; set; }
-
-        private string nameActeur;
-
-
 
 
     }
